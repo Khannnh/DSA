f28e036c21051dc535695c44d6412a1f80c0b7dc
Khannnh <hhonksnguyen2205@gmail.com>
2026-07-16 00:37:55 +0700

    tập quân sự ( sinh tập con kế tiếp

--- DSA01010.py
+++ DSA01010.py
@@ -1,2 +1,27 @@
 # sinh tổ hợp chập k của n phần tử theo thứ tự từ điển
-#chương mỹ resort 
+# tìm tập con tiếp theo , so sánh xem số nào ko lặp lại ở tập con tiếp theo 
+def tohoptieptheo(n,k,a): 
+    pos = -1 
+    next_a = a.copy()
+    for i in range(k-1 ,-1 ,-1):
+        if next_a[i] < n-k+i+1 : 
+            pos = i 
+            next_a[i] += 1 
+            break
+    # nếu là cấu hình cuối cùng 
+    if pos == -1 :
+        return k
+    #xử lý đằng sau pos tăng lên tối thiểu(min)
+    for j in range(pos+1,k):
+        next_a[j] = next_a[j-1]+1
+    
+    cnt = 0 
+    for x in a: 
+        if x not in next_a:
+           cnt += 1
+    return cnt  
+t= int(input())
+for _ in range(t): 
+    n,k = list(map(int , input().split()))
+    x = list(map(int, input().split()))
+    print(tohoptieptheo(n,k,x))
