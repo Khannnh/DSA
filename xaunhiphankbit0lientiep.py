d91414edeea8e7834bfd2cd3280162fe24bf2f13
Khannnh <hhonksnguyen2205@gmail.com>
2026-07-17 19:58:40 +0700

    xâu nhị phân k bit 1

--- xaunhiphankbit0lientiep.py
+++ xaunhiphankbit0lientiep.py
@@ -1,24 +1,12 @@
-T = int(input())
-for _ in range(T):
-    N, K = map(int, input().split())
-
-    pos = [0] * K
-    res = []
-
-    def backtrack(i, start):
-        if i == K:
-            s = ['0'] * N
-            for p in pos:
-                s[p] = '1'
-            res.append(''.join(s))
-            return
-
-        for p in range(start, N):
-            pos[i] = p
-            backtrack(i + 1, p + 1)
-
-    backtrack(0, 0)
-
-    # in ngược để ra thứ tự từ điển tăng
-    for s in reversed(res):
-        print(s)
+import itertools 
+# sinh xâu nhị phân k bit 1 theo thứ tự tăng dần 
+t=int(input())
+for _ in range(t): 
+    n,k = list(map(int , input().split()))
+    slbit0 = n-k 
+    vi_tri_bit_0 = itertools.combinations(range(n) , n-k)
+    for vt in vi_tri_bit_0: # vt (0,1,2,3)
+        a = [1]*n
+        for i in vt : 
+            a[i] = 0 
+        print(''.join(map(str,a)))   
