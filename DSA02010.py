37edfe76a78bff79a3dc29dfe611c8e9a291f6ae
Khannnh <hhonksnguyen2205@gmail.com>
2026-07-24 13:58:24 +0700

    bài có rất nhiều note 🥲🥲🥲‼️‼️‼️‼️‼️

---
+++ DSA02010.py
@@ -0,0 +1,34 @@
+#🥲🙄 bài này có quá nhiều note ‼️‼️‼️
+#tổ hợp số có tổng bằng x 
+def dfs(rem_x:int , a:list , start_ids:int ,path:list, result:list):
+    n=len(a)
+    #basecase: nếu tổng rem_x lùi về 0 => sinh hết 1 tổ hợp 
+    if rem_x==0 : 
+        result.append(list(path))  # FIX: Tạo bản sao list(path) thay vì dùng path gốc
+        return 
+    for i in range(start_ids,n):
+        #cắt nhánh sớm a[i]>rem_x : dừng luôn vì dãy a sắp xếp bé-> lớn
+        if a[i]> rem_x:
+            break 
+        path.append(a[i])
+        dfs(rem_x-a[i] ,a, i , path,result)
+        path.pop()  #khôi phục trạng thái 
+t=int(input())
+for _ in range(t):
+    N, X = map(int , input().split())
+    a= list(map(int , input().split()))
+    a.sort()
+    result = []
+    path=[]
+    dfs(X, a, 0 , path, result)
+    if not result : 
+        print(-1)
+    else : 
+        # Format lại kết quả đúng chuẩn [2 2 2 2][2 2 4]...
+        out=[] # nếu in luôn result thì ko đúng format 🙄
+        for p in result: 
+            path= ' '.join(map(str,p))
+            out.append("[" + path + "]")
+        print("".join(map(str,out))) 
+        #bài này dùng *out thì ko đc vì giữa ] và [ có dấu cách :))))
+        #thế là phải dùng join 🙄
