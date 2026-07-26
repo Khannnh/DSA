f246020d781c6c71e20ce26278449a8a4b4e5dc5
Khannnh <hhonksnguyen2205@gmail.com>
2026-07-26 11:43:13 +0700

    me cung DR ＞﹏＜

---
+++ DSA02003.py
@@ -0,0 +1,80 @@
+# Các hướng di chuyển
+DIRECTIONS = [
+    (1, 0, 'D'),   # Down
+    (0, 1, 'R')    # Right
+]
+# DFS / Backtracking
+# ==========================
+def dfs(
+    maze: list[list[int]],
+    x: int,
+    y: int,
+    path: list[str],
+    result: list[str]
+) -> None:
+    n = len(maze)
+    # 1. Base case
+    if x == n - 1 and y == n - 1:
+        result.append("".join(path))
+        return
+
+    # 2. Sinh các lựa chọn
+    for direction in DIRECTIONS:
+        dx = direction[0]
+        dy = direction[1]
+        move = direction[2]
+
+        # 3. Tính trạng thái mới
+        next_x = x + dx
+        next_y = y + dy
+
+        # 4. Kiểm tra hợp lệ
+        # Ra ngoài mê cung
+        if next_x < 0 or next_x >= n:
+            continue
+        if next_y < 0 or next_y >= n:
+            continue
+        # Gặp tường
+        if maze[next_x][next_y] == 0:
+            continue
+        # 5. Chọn
+        path.append(move)
+        # 6. Đệ quy
+        dfs(
+            maze,
+            next_x,
+            next_y,
+            path,
+            result
+        )
+        # 7. Hoàn tác
+        path.pop()
+# Giải một test
+# ==========================
+def solve(maze: list[list[int]]) -> list[str]:
+    result = []
+    # Không có điểm xuất phát
+    if maze[0][0] == 0:
+        return result
+    dfs(
+        maze=maze,
+        x=0,
+        y=0,
+        path=[],
+        result=result
+    )
+    return result
+
+# Main
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    maze = []
+    for i in range(n):
+        row = list(map(int, input().split()))
+        maze.append(row)
+    res = solve(maze)
+    if len(res) == 0:
+        print(-1)
+    else:
+        print(*res)
